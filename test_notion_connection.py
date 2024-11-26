ef8d6fb55f1376133b5488eeb2343e5b304df341
saleh@arch <saleh.alsyefi@gmail.com>
2024-11-26 11:28:12 +0000

    ...

--- test_notion_connection.py
+++ test_notion_connection.py
@@ -1,11 +1,11 @@
 import aiohttp
 import asyncio
-from globals import NOTION_API_TOKEN, PHD_MILESTONES_DATABASE_ID
+from globals import NOTION_API_TOKEN, NOTION_DATABASE_ID
 
 headers = {"Authorization": f"Bearer {NOTION_API_TOKEN}", "Content-Type": "application/json", "Notion-Version": "2022-06-28"}
 
 async def test_fetch():
-    url = f"https://api.notion.com/v1/databases/{PHD_MILESTONES_DATABASE_ID}/query"
+    url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
     async with aiohttp.ClientSession() as session:
         async with session.post(url, headers=headers, json={}) as response:
             print(f"Status: {response.status}")
